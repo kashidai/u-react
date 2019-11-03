6573e1a47142f7ccc31ad9abfb649fa673fbf11f
Dai <d.k.95.82.8192@icloud.com>
2019-11-03 02:07:50 -0800

    create components

--- u-react/src/App.js
+++ u-react/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
 
+// class App extends Component {
+//     render() {
+//         return (
+//             <React.Fragment>
+//                 <label htmlFor= "bar" >bar</label>
+//                 <input type="text" onChange={() => {console.log("Heyyyyyy")}}/>
+//             </React.Fragment>
+//         )
+//     }
+//
+// }
+
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>I'm App components</h1>
+            <Cat />
+        </div>
+    );
+}
+
+const Cat = () =>{
+    return <div>I'm Cat components</div>
 }
 
 export default App;
