0e282d853c6d7087e2970a7984d3cfe12a9354b3
Dai <d.k.95.82.8192@icloud.com>
2019-11-05 23:32:13 -0800

    props

--- u-react/src/App.js
+++ u-react/src/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-        <p>Heuuuuuu</p>
-    </div>
-  );
+
+const App = () => {
+    const profiles = [
+        {name: "Taro", age: 10},
+        {name: "Hanako", age: 5},
+        {name: "Noname"},
+
+    ]
+
+    return (
+        <div>
+            {
+                profiles.map((profiles, index) => {
+                    return <User name={profiles.name} age={profiles.age} key={index}/>
+                })
+            }
+        </div>
+    )
 }
-.
+
+const User = (props) => {
+    return <div>Hi, I'm {props.name}! {props.age} years old!</div>
+}
+
+User.defaultProps = {
+    age: 1
+}
+
+
 export default App;
