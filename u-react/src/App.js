4f4e6c0b3bcdac393dc9f46dc3c07d9d210f7e1a
Dai <d.k.95.82.8192@icloud.com>
2019-11-05 23:49:07 -0800

    propTypes

--- u-react/src/App.js
+++ u-react/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 const App = () => {
     const profiles = [
         {name: "Taro", age: 10},
         {name: "Hanako", age: 5},
-        {name: "Noname"},
+        {name: "Noname", age: 3},
 
     ]
 
@@ -24,8 +25,9 @@ const User = (props) => {
     return <div>Hi, I'm {props.name}! {props.age} years old!</div>
 }
 
-User.defaultProps = {
-    age: 1
+User.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired
 }
 
 
