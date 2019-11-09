6441e4a925ec2153e7b1cc20aecd0f995c6654d8
Dai <d.k.95.82.8192@icloud.com>
2019-11-08 19:11:15 -0800

    aaa

--- u-react/src/App.js
+++ u-react/src/App.js
@@ -1,15 +1,58 @@
 import React from 'react';
 
 
-function App() {
-    return (
-        <div>
-          state
-        </div>
-    )
+class App　extends　React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    //    bind func
+
+    }
+
+
+    printParam = () => {
+        console.log(this);
+    }
+ß
+    printParamB () {
+        console.log(this.printParam);
+        const A = () => {
+           console.log(this);
+       }
+       A();
+    }
+
+    printParamC = () => {
+        console.log("");
+    }
+
+    render(){
+        let param = 'global param';
+
+        // let object1 = {
+        //     param: 'object111111',
+        //     func: () => this.printParam('object111111')
+        // }
+        // let object2 = {
+        //     param: 'object2222222222',
+        //     func: this.printParam
+        // }
+
+        return (
+            <div>
+                state
+                <button onClick={this.printParam}>object1</button>
+                <button onClick={this.printParamB}>object2</button>
+            </div>
+        )
+    }
+
 }
 
-const  User = () =>{
+const User = () => {
     return <div>props</div>
 }
 
